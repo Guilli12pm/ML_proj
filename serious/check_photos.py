35c973030a01dfdff0e04aefafdb8a0ba8e53da2
guilli12pm <williampm@pontmirabeau.com>
2019-05-03 22:38:25 +0200

    Changes

--- serious/check_photos.py
+++ serious/check_photos.py
@@ -4,6 +4,7 @@ import os
 from eyes_check import check_eyes
 import shutil
 
+"""
 directory = 'pic/'
 for filename in os.listdir(directory):
     if filename.endswith(".png"):
@@ -12,3 +13,23 @@ for filename in os.listdir(directory):
             shutil.move("pic/"+filename, "next_ite/")
         else:
             print(filename, "bad")
+
+"""
+
+directory = 'Database/'
+
+print(os.listdir(directory))
+
+#print(os.listdir(directory + os.listdir(directory)[0]))
+
+for names in os.listdir(directory):
+    if os.path.isdir(directory + names):
+        os.mkdir("next_ite/" + names + "/")
+        for filename in os.listdir(directory + names + "/"):
+            if filename.endswith(".png"):
+                if check_eyes(directory + names + "/",filename):
+                    print(filename, "good")
+                    shutil.move(directory + names + "/" + filename, "next_ite/" + names + "/")
+                else:
+                    print(filename, "bad")
+
--- serious/eyes_check.py
+++ serious/eyes_check.py
@@ -3,11 +3,11 @@ import cv2
 from PIL import ImageGrab
 
 
-def check_eyes(name):
+def check_eyes(directory,name):
 
   eyeCascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
-  frame = cv2.imread("pic/"+name)
+  frame = cv2.imread(directory + name)
 
   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
