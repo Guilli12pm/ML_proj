ed5a89ecf8291f182f4b44d2f067715a3ca55c3a
Guillaume.Loranchet <guillaume.loranchet@polytechnique.edu>
2019-06-03 01:36:18 +0200

    add new_pic(1) and (2)

--- make_tar.py
+++ make_tar.py
@@ -3,3 +3,6 @@
 import os
 
 os.system("tar -zcvf next_ite.tar.gz next_ite")
+os.system("tar -zcvf new_pic.tar.gz new_pic")
+os.system("tar -zcvf new_pic(1).tar.gz new_pic(1)")
+os.system("tar -zcvf new_pic(2).tar.gz new_pic(2)")
