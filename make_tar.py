8bb10dc913d1ba8be83b73486e8a7192e7eb592f
Guillaume.Loranchet <guillaume.loranchet@polytechnique.edu>
2019-06-03 14:44:48 +0200

    new pictures for testing

--- make_tar.py
+++ make_tar.py
@@ -2,7 +2,7 @@
 
 import os
 
-os.system("tar -zcvf next_ite.tar.gz next_ite")
-os.system("tar -zcvf new_pic.tar.gz new_pic")
-os.system("tar -zcvf new_pic(1).tar.gz new_pic(1)")
-os.system("tar -zcvf new_pic(2).tar.gz new_pic(2)")
+os.system("tar -zcvf result.tar.gz next_ite")
+#os.system("tar -zcvf new_pic.tar.gz new_pic")
+#os.system("tar -zcvf new_pic(1).tar.gz new_pic(1)")
+#os.system("tar -zcvf new_pic(2).tar.gz new_pic(2)")
