99e480394773d794930147b8aface535b6507bd4
Guillaume.Loranchet <guillaume.loranchet@polytechnique.edu>
2019-06-04 23:29:09 +0200

    add pictures konstantinos

--- Noise.py
+++ Noise.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import os
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 import argparse
 import imutils
 import random
@@ -13,7 +13,7 @@ def sp_noise(image,prob):
     prob: Probability of the noise 0.01-0.1
     '''
     output = np.zeros(image.shape,np.uint8)
-    thres = 1 - prob 
+    thres = 1 - prob
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             rdn = random.random()
@@ -25,18 +25,19 @@ def sp_noise(image,prob):
                 output[i][j] = image[i][j]
     return output
 
-for names in os.listdir("next_ite"):
+for names in ['Konstantinos_A']: #os.listdir("next_ite"):
     if names != ".DS_Store":
         print("Working on:", names)
         os.mkdir("new_pic/"+names)
         os.mkdir("normal_pic/"+names)
         li = os.listdir("next_ite/"+names)
-        li.remove(".DS_Store")
+        if ".DS_Store" in li:
+            li.remove(".DS_Store")
         random.shuffle(li)
         for i in range(10):
-            copyfile("next_ite/"+names+"/"+li[i],"new_pic/"+names + "/"+li[i])            
-            copyfile("next_ite/"+names+"/"+li[i],"normal_pic/"+names + "/"+li[i])      
-            # Blurring 
+            copyfile("next_ite/"+names+"/"+li[i],"new_pic/"+names + "/"+li[i])
+            copyfile("next_ite/"+names+"/"+li[i],"normal_pic/"+names + "/"+li[i])
+            # Blurring
             img = cv2.imread("next_ite/" + names + "/" + li[i])
             kernel = np.ones((5,5),np.float32)/25
             dst = cv2.filter2D(img,-1,kernel)
@@ -44,14 +45,14 @@ for names in os.listdir("next_ite"):
 
             # Sharpening
             img1 = cv2.imread("next_ite/" + names + "/" + li[i])
-            kernel_sharpening1 = np.array([[-1,-1,-1], 
+            kernel_sharpening1 = np.array([[-1,-1,-1],
                               [-1, 9,-1],
                               [-1,-1,-1]])
             sharpened1 = cv2.filter2D(img1, -1, kernel_sharpening1)
             cv2.imwrite("new_pic/"+names+"/"+li[i].strip(".png") + "_sharpened_1_" + str(i) + ".png",sharpened1)
 
             img2 = cv2.imread("next_ite/" + names + "/" + li[i])
-            kernel_sharpening2 = np.array([[-1,-1,-1], 
+            kernel_sharpening2 = np.array([[-1,-1,-1],
                               [-1, 10,-1],
                               [-1,-1,-1]])
             sharpened2 = cv2.filter2D(img2, -1, kernel_sharpening2)
@@ -76,7 +77,3 @@ for names in os.listdir("next_ite"):
                     rotated = imutils.rotate_bound(image, angle)
                     cv2.imwrite("new_pic/"+names+"/"+im.strip(".png") + "_rot_clock_" + str(j) + ".png",rotated)
                     j += 1
-
-
-            
-
