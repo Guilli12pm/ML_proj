7543ff50a322961c912c110489b356ee26c350dc
Guillaume.Loranchet <guillaume.loranchet@polytechnique.edu>
2019-06-04 18:27:50 +0200

    new pictures

--- make_tar.py
+++ make_tar.py
@@ -3,6 +3,7 @@
 import os
 
 os.system("tar -zcvf result.tar.gz next_ite")
+os.system("tar -zcvf normal_pic.tar.gz normal_pic")
 #os.system("tar -zcvf new_pic.tar.gz new_pic")
 #os.system("tar -zcvf new_pic(1).tar.gz new_pic(1)")
 #os.system("tar -zcvf new_pic(2).tar.gz new_pic(2)")
